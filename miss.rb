7c51f76567ba5b29a9867d078b8b99734904c0b2
FUJIWARA Shohei <head@Altrouge.local>
2014-10-28 01:20:09 +0900

    test

---
+++ miss.rb
@@ -0,0 +1,20 @@
+class A
+  def initialize(x)
+    @x = x
+  end
+
+  def m
+    @x
+  end
+  
+  def method_missing(name, *args)
+    p name
+    p args
+    super.method_missing(name, args)
+  end
+
+end
+
+a = A.new 1
+
+a.nla 1,1,nil,"Foo"
